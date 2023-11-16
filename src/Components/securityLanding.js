d64c668b3e7de5a1a38bf507c4cd55a9ce3a88a7
Aylin <58498498+CAMAY3101@users.noreply.github.com>
2023-11-15 20:33:26 -0600

    Skeleton for elements

--- src/Components/securityLanding.js
+++ src/Components/securityLanding.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
+import secuirtyimage from '../Assets/seguridad.png'
+
 function securityLandings() {
   return (
     <div className="flex flex-col md:flex-row">
       <div className="w-full md:w-1/2 flex justify-center items-center">
-        <img className="w-3/4" src="https://source.unsplash.com/random" alt="Random Image" />
+        <img className="w-3/4" src={secuirtyimage} alt="Random Image" />
       </div>
       <div className="w-full md:w-1/2 md:ml-4">
-        <h2 className="text-2xl font-bold mb-4">Subtitle</h2>
-        <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec aliquet magna vitae risus viverra, ac mattis justo malesuada. Suspendisse potenti. Ut id sem sit amet metus tincidunt aliquam. Vestibulum feugiat urna at fringilla commodo.</p>
+        <div className="text-2xl font-bold mb-4">
+          <span>Burs ai tu espacio seguro</span>
+          <span> Confiamos en ti </span> <span>y </span><span>tú</span> <span>puedes </span>
+          <span>confiar en nosotros.</span>
+        </div>
+        <p className="text-gray-700"> Jamás tendremos practicas de cobranza abusivas ni buscaremos a tus contactos ni referidos</p>
       </div>
     </div>
   )
