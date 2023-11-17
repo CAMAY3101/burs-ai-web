fae94fba8f7201c13d282541c3bcdc4082133859
Aylin <58498498+CAMAY3101@users.noreply.github.com>
2023-11-17 16:30:23 -0600

    Skeleton footer 1
    Fixes #25

--- src/Components/infoFooter.js
+++ src/Components/infoFooter.js
@@ -1,41 +1,32 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
+import mapIcon from '../Assets/map.png'
+import phoneIcon from '../Assets/phone.png'
+import emailIcon from '../Assets/email.png'
 
 const InfoFooter = () => {
-    const [isColumn, setIsColumn] = useState(false);
-
-    const handleResize = () => {
-        setIsColumn(window.innerWidth <= 768);
-    };
-
-    useEffect(() => {
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
     // <div className={`flex ${isColumn ? 'flex-col' : 'flex-row'}`}>
 
     return (
         <div className="flex flex-col md:flex-row justify-center ">
             <div className="w-full md:w-1/3 p-4 bg-gray-200 flex items-center">
-                <img src="https://source.unsplash.com/random" alt="Subcontainer 1" className="w-16 h-16 mr-4" />
+                <img src={mapIcon} alt="Subcontainer 1" className="w-20 h-20 mr-4" />
                 <div>
-                    <h2 className="text-lg font-semibold">Subcontainer 1 Title</h2>
-                    <p className="text-gray-600">Subcontainer 1 Subtitle</p>
+                    <h2 className="text-lg font-semibold">Dirección</h2>
+                    <p className="text-gray-600">Av. de las Américas 1545 piso 20, Providencia, 44630 Guadalajara, Jal.</p>
                 </div>
             </div>
             <div className="w-full md:w-1/3 p-4 bg-gray-300 flex items-center">
-                <img src="https://source.unsplash.com/random" alt="Subcontainer 1" className="w-16 h-16 mr-4" />
+                <img src={phoneIcon} alt="Subcontainer 1" className="w-20 h-20 mr-4" />
                 <div>
-                    <h2 className="text-lg font-semibold">Subcontainer 1 Title</h2>
+                    <h2 className="text-lg font-semibold">Numero de atención 24/7</h2>
                     <p className="text-gray-600">Subcontainer 1 Subtitle</p>
                 </div>
             </div>
             <div className="w-full md:w-1/3 p-4 bg-gray-400 flex items-center">
-                <img src="https://source.unsplash.com/random" alt="Subcontainer 1" className="w-16 h-16 mr-4" />
+                <img src={emailIcon} alt="Subcontainer 1" className="w-20 h-20 mr-4" />
                 <div>
-                    <h2 className="text-lg font-semibold">Subcontainer 1 Title</h2>
-                    <p className="text-gray-600">Subcontainer 1 Subtitle</p>
+                    <h2 className="text-lg font-semibold">E-Mail de atención 24/7 </h2>
+                    <p className="text-gray-600">soporte@aiburs.com</p>
                 </div>
             </div>
         </div>
