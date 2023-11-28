c7f4bed1e88e1be9b8a1e39d02d0b33a635ae6de
Aylin <58498498+CAMAY3101@users.noreply.github.com>
2023-11-28 14:51:23 -0600

    Fixes

--- src/Components/QuieroInvertir/heroQI.js
+++ src/Components/QuieroInvertir/heroQI.js
@@ -5,22 +5,18 @@ import '../../Styles/QuieroInvertir.scss'
 function heroQI() {
   return (
     <div id='proceso'>
-        <div className="bg-cover bg-center h-screen flex items-center justify-end cover-hero">
-            <div className="flex flex-col md:flex-row justify-end">
-                <div className="p-10">
-                    <div className="mb-10 text-center">
-                          <span className='invierte'>
-                              INVIERTE<br />
-                          </span>
-                          <span className='y-ve-crecer-tu-dinero-con'>
-                              Y VE CRECER <br />
-                              TU DINERO CON <br />
-                          </span>
-                          <span className='burs'>
-                              BURS
-                          </span>
-                    </div>
-                </div>
+        <div className="bg-cover bg-center flex items-center justify-end cover-hero-QI">
+            <div className="wflex flex-col md:flex-row justify-end text-center">
+                <span className='invierte'>
+                    INVIERTE<br />
+                </span>
+                <span className='y-ve-crecer-tu-dinero-con'>
+                    Y VE CRECER <br />
+                    TU DINERO CON <br />
+                </span>
+                <span className='burs'>
+                    BURS
+                </span>
             </div>
         </div>
     </div>
