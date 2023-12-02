60faa390f9863ea75e4e29683ee978b3165e4fa6
Aylin Camacho <58498498+CAMAY3101@users.noreply.github.com>
2023-12-01 23:10:47 -0600

    Revert "31 333 create simulador component"

--- src/Pages/QuieroInvertir.js
+++ src/Pages/QuieroInvertir.js
@@ -2,15 +2,12 @@ import React from 'react'
 
 import HeroQI from '../Components/QuieroInvertir/heroQI'
 import Invertir from '../Components/QuieroInvertir/Invertir'
-import Simulador from '../Components/QuieroInvertir/Simulador'
-
 
 function QuieroInvertir() {
   return (
     <div>
       <HeroQI />
       <Invertir />
-      <Simulador />
     </div>
   )
 }
