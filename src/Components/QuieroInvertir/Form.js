f4d7588aae571eba5b08ade3f0909adfb25d0caf
Aylin <58498498+CAMAY3101@users.noreply.github.com>
2023-12-03 22:30:01 -0600

    Form Skeleton
    Fixes #52

--- src/Components/QuieroInvertir/Form.js
+++ src/Components/QuieroInvertir/Form.js
@@ -12,19 +12,19 @@ function Form() {
             </div>
               
             <div id='form' className='w-8/12 flex flex-col items-center'>
-                <h2 className="text-2xl font-bold mb-6">Contact Form</h2>
+                <h2 className="text-2xl font-bold mb-6">Contactanos</h2>
                 <div className="w-10/12 space-y-8 justify-center">
-                    <Input type="name" label="Name" color='primary' />
+                    <Input type="name" label="Nombre Completo" placeholder='Ejemplo: Esteban Garcia Garcia' color='primary' />
                     <div className="flex md:flex-row space-x-8 ">
-                        <Input type="email" label="Email" color='primary' />
-                        <Input type="tel" label="Telephone" color='primary' />
+                        <Input type="email" label="Correo Electronico" placeholder='ejemplo@outlook.com' color='primary' />
+                        <Input type="tel" label="Telefono" placeholder='Ejemplo: (33) 3333 3333' color='primary' />
                     </div>
-                    <Textarea label="Description" color='primary'/>
+                    <Textarea label="Mensaje" color='primary'/>
                     <button
                         type="submit"
                         className="w-full bg-black text-white py-2 px-4 rounded-lg font-bold uppercase hover:bg-gray-800"
                     >
-                        Send Message
+                        Enviar Mensaje
                     </button>
                 </div>
             </div>
@@ -32,5 +32,6 @@ function Form() {
     </div>
   )
 }
+
 
 export default Form
