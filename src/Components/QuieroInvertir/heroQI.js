84f2f97f871fe7b74d2ba6bf1dc56e169871f370
Aylin <58498498+CAMAY3101@users.noreply.github.com>
2023-12-06 15:44:43 -0600

    Refactor HeroQI
    - Responsivo para mobile, tablet and web
    - Sustituir clases con tailwind

--- src/Components/QuieroInvertir/heroQI.js
+++ src/Components/QuieroInvertir/heroQI.js
@@ -5,18 +5,20 @@ import '../../Styles/QuieroInvertir.scss'
 function heroQI() {
   return (
     <div id='proceso'>
-        <div className="bg-cover bg-center flex items-center justify-end mb-12 cover-hero-QI">
-            <div className="wflex flex-col md:flex-row justify-end text-center">
-                <span className='invierte'>
+        <div className="bg-cover bg-center h-screen flex items-center justify-center px-10 mb-10 lg:justify-end md:px-10 lg:px-40 cover-hero-QI">
+            <div className="flex flex-col justify-end text-center space-y-5 md:space-y-8">
+                <h3 className='text-persian-rose-600 rubik-Bold-70 md:text-[110px]'>
                     INVIERTE<br />
-                </span>
-                <span className='y-ve-crecer-tu-dinero-con'>
+                </h3>
+                <h3 className='text-dark-blue-50 rubik-Bold-32 md:text-[50px]'>
                     Y VE CRECER <br />
-                    TU DINERO CON <br />
-                </span>
-                <span className='burs'>
+                </h3>
+                <h3 className='text-dark-blue-50 rubik-Bold-32 md:text-[50px]'>
+                    TU DINERO <br />
+                </h3>
+                <h3 className='text-persian-rose-50 rubik-Bold-88'>
                     BURS
-                </span>
+                </h3>
             </div>
         </div>
     </div>
