124bb47fd5fcc4c35bd195b5bf65f0d071a05d13
Aylin <58498498+CAMAY3101@users.noreply.github.com>
2023-12-12 10:33:23 -0600

    Final Fixes

--- src/Components/QuieroInvertir/heroQI.js
+++ src/Components/QuieroInvertir/heroQI.js
@@ -6,17 +6,17 @@ function heroQI() {
   return (
     <div id='proceso'>
         <div className="bg-cover bg-center h-screen flex items-center justify-center px-10 mb-10 lg:justify-end md:px-10 lg:px-40 cover-hero-QI">
-            <div className="flex flex-col justify-end text-center space-y-5 md:space-y-8">
-                <h3 className='text-persian-rose-600 rubik-Bold-70 md:text-[110px]'>
+            <div className="flex flex-col justify-end text-center space-y-5 md:space-y-10">
+                  <h3 className='bg-persian-rose-600/60 text-persian-rose-100 rubik-ExtraBold-70 tracking-wider md:text-[95px]'>
                     INVIERTE<br />
                 </h3>
-                <h3 className='text-dark-blue-50 rubik-Bold-32 md:text-[50px]'>
+                <h3 className='text-dark-blue-50 rubik-ExtraBold-32 tracking-wider md:text-[50px]'>
                     Y VE CRECER <br />
                 </h3>
-                <h3 className='text-dark-blue-50 rubik-Bold-32 md:text-[50px]'>
+                <h3 className='text-dark-blue-50 rubik-ExtraBold-32 tracking-wider md:text-[50px]'>
                     TU DINERO <br />
                 </h3>
-                <h3 className='text-persian-rose-50 rubik-Bold-88'>
+                <h3 className='text-persian-rose-50 rubik-ExtraBold-88 tracking-wider md:text-[105px]'>
                     BURS
                 </h3>
             </div>
