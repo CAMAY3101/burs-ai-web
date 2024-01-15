2e7bd10938fec9222269824c1d74a68edfe0282d
Aylin <58498498+CAMAY3101@users.noreply.github.com>
2024-01-15 11:30:13 -0600

    Set up for component

---
+++ src/Components/SolicitarPrestamo/FormDatosUsuario.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Input, Textarea, Button } from "@nextui-org/react";
+
+import "../../Styles/SolicitarPrestamo.scss"
+
+const styles = {
+    input: [
+        "rubik-Regular-15",
+        "text-dark-blue-800",
+        "placeholder:text-dark-blue-200",
+    ],
+    inputWrapper: [
+        "rounded-3xl",
+        "border-dark-blue-400",
+        "data-[hover=true]:border-dark-blue-700",
+        "group-data-[focus=true]:border-dark-blue-900",
+        "!cursor-text",
+    ]
+}
+
+function FormDatosUsuario() {
+  return (
+    <div></div>
+  )
+}
+
+export default FormDatosUsuario
