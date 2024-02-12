908350ad3e18ac10d782d4e633f35f0cf0f11ec6
Aylin <58498498+CAMAY3101@users.noreply.github.com>
2024-02-12 11:50:32 -0600

    Skeleton
    Fixes #93

--- src/Pages/CreaTuCuenta.js
+++ src/Pages/CreaTuCuenta.js
@@ -9,39 +9,39 @@ function CreaTuCuenta() {
     <div className='flex flex-col items-center space-y-8 pt-4'>
         <div className='flex flex-col items-center'>
             <img className='w-24' alt='icon-color-burs' src={bursColorIcon}/>
-            <h1>Sign Up</h1>
+            <h1>Crea tu cuenta</h1>
         </div>
         <div className='w-10/12 space-y-6'>
             <Input
                 type='email'
-                label= 'email'
-                placeholder='example@outlook.com'
+                label= 'Correo Electrónico'
+                placeholder='ejemplo@outlook.com'
                 size='md'
                 variant='bordered'
             />
             <div className='space-y-2'>
                 <Input
                     type='password'
-                    label='password'
-                    placeholder='Enter password'
+                    label='Contraseña'
+                    placeholder='Ingresa contraseña'
                     size='md'
                     variant='bordered'
                 />
                 <ul className="text-sm">
                     <li>
-                        Password should be at least 8 characters long
+                        Contraseña debe tener mínimo 8 caracteres. 
                     </li>
                     <li >
-                        Password should contain at least one lowercase letter
+                        Al menos una letra minúscula 
                     </li>
                     <li >
-                        Password should contain at least one uppercase letter
+                        Al menos una letra mayúscula.
                     </li>
                     <li >
-                        Password should contain at least one number
+                        Al menos un número 
                     </li>
                     <li >
-                        Password should contain at least one special character
+                        Al menos un caracter especial
                     </li>
                 </ul>
             </div>
