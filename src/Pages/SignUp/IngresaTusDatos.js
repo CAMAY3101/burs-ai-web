4b5160f90df88c47beece22070811f257188173c
Aylin <58498498+CAMAY3101@users.noreply.github.com>
2024-02-13 17:43:05 -0600

    Skeleton
    Fixes #101

--- src/Pages/SignUp/IngresaTusDatos.js
+++ src/Pages/SignUp/IngresaTusDatos.js
@@ -10,7 +10,7 @@ function IngresaTusDatos() {
                 <span class="flex items-center justify-center w-5 h-5 me-2 text-xs border border-blue-600 rounded-full shrink-0 dark:border-blue-500">
                     1
                 </span>
-                Personal <span class="hidden sm:inline-flex sm:ms-2">Info</span>
+                  Registro <span class="hidden sm:inline-flex sm:ms-2">de Datos</span>
                 <svg class="w-3 h-3 ms-2 sm:ms-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7 9 4-4-4-4M1 9l4-4-4-4" />
                 </svg>
@@ -19,7 +19,7 @@ function IngresaTusDatos() {
                 <span class="flex items-center justify-center w-5 h-5 me-2 text-xs border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
                     2
                 </span>
-                Account <span class="hidden sm:inline-flex sm:ms-2">Info</span>
+                    Verificación <span class="hidden sm:inline-flex sm:ms-2">de Datos</span>
                 <svg class="w-3 h-3 ms-2 sm:ms-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m7 9 4-4-4-4M1 9l4-4-4-4" />
                 </svg>
@@ -28,17 +28,17 @@ function IngresaTusDatos() {
                 <span class="flex items-center justify-center w-5 h-5 me-2 text-xs border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
                     3
                 </span>
-                Review
+                  Solicitar <span class="hidden sm:inline-flex sm:ms-2">Prestamo</span>
             </li>
         </ol>
 
         <div className='w-11/12 flex flex-col space-y-5'>
-            <h1> Tittle</h1>
+            <h1> Ingresa tus datos </h1>
             <Input
                 isRequired
                 type='password'
-                label='Name'
-                placeholder='Enter Name'
+                label='Nombre(s)'
+                placeholder='Ejemplo: Juan'
                 size='md'
                 variant='bordered'
                 
@@ -46,24 +46,16 @@ function IngresaTusDatos() {
             <Input
                 isRequired
                 type='password'
-                label='Name'
-                placeholder='Enter Name'
+                label='Edad'
+                placeholder='Ej: 25'
                 size='md'
                 variant='bordered'
             />
             <Input
                 isRequired
                 type='password'
-                label='Name'
-                placeholder='Enter Name'
-                size='md'
-                variant='bordered'
-            />
-            <Input 
-                isRequired
-                type='password'
-                label='Name'
-                placeholder='Enter Name'
+                label='Telefono'
+                placeholder='Ej: 55 1234 5678'
                 size='md'
                 variant='bordered'
             />
