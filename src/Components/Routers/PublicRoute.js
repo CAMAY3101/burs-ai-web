782b304d381da347d48f41be346b23ed62d918a1
Aylin <58498498+CAMAY3101@users.noreply.github.com>
2024-09-04 19:53:53 -0600

    Update PublicRoute.js

--- src/Components/Routers/PublicRoute.js
+++ src/Components/Routers/PublicRoute.js
@@ -6,10 +6,6 @@ import Solicitar from '../../Pages/SolicitarPrestamo/Solicitar';
 export default function PublicRoute() {
     const { tokenExist, verificationStep } = useAuthContext();
 
-    // if user is trying to access  to QUIERO_INVERTIR or / path let him in even if he is not authenticated
-    if ( window.location.pathname === '/') {
-        return <Outlet />;
-    }
     if (tokenExist) {
         if (verificationStep >= 1) {
             return (
