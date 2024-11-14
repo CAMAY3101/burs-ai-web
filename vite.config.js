88a5c021aa72c24c0971e15f91772e518a2053fa
Aylin <58498498+CAMAY3101@users.noreply.github.com>
2024-11-14 16:47:06 -0600

    vite migration

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig, transformWithEsbuild } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "tailwindcss";
+
+export default defineConfig({
+    plugins: [
+        react(),
+        {
+            name: 'load+transform-js-files-as-jsx',
+            async transform(code, id) {
+                if (!id.match(/src\/.*\.js$/)) {
+                    return null;
+                }
+
+                // Use the exposed transform from vite, instead of directly
+                // transforming with esbuild
+                return transformWithEsbuild(code, id, {
+                    loader: 'jsx',
+                    jsx: 'automatic', // 👈 this is important
+                });
+            },
+        },
+    ],
+    css: {
+        postcss: {
+            plugins: [tailwindcss()],
+        },
+    },
+    // Workaround before renaming .js to .jsx
+    optimizeDeps: {
+        esbuildOptions: {
+            loader: {
+                '.js': 'jsx',
+            },
+        },
+    },
+    server: {
+        port: 3000,
+        open: true,
+    },
+})
